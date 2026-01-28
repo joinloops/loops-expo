2233ae1088c4b6b5ca339484f6fa96477437f556
Daniel Supernault <danielsupernault@gmail.com>
2026-01-28 02:45:05 -0700

    Update index.tsx

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -282,7 +282,6 @@ export default function LoopsFeed({ navigation }) {
             selectedVideo,
             screenFocused,
             videoPlaybackRates,
-            navigation,
         ],
     );
 
