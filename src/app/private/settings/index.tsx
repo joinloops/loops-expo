61711a018ec1b5ffad06404a4aa82d529cc20d72
Daniel Supernault <danielsupernault@gmail.com>
2026-02-26 05:21:59 -0700

    Bump version

--- src/app/private/settings/index.tsx
+++ src/app/private/settings/index.tsx
@@ -161,7 +161,7 @@ export default function SettingsScreen() {
                     onPress={() => handleSignOut()}
                 />
                 <View style={tw`flex justify-center items-center mt-5 mb-20`}>
-                    <Text style={tw`text-gray-500`}>Loops v1.0.1.24</Text>
+                    <Text style={tw`text-gray-500`}>Loops v1.0.1.25</Text>
                 </View>
             </ScrollView>
         </View>
