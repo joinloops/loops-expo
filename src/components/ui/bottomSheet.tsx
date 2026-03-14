c40dd808e7c5c53d59d29f84f1f8e1475cf5d6f5
Alexandre Tachau <alexandre.tachau@tutamail.com>
2026-03-14 15:34:37 +0100

    BottomSheet: content height calculated + max height is 85% of screen

--- src/components/ui/bottomSheet.tsx
+++ src/components/ui/bottomSheet.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from '@/contexts/ThemeContext';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dimensions, Modal, Pressable, Share, Text, TouchableOpacity, View } from 'react-native';
 import { Gesture, GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler';
 import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
@@ -15,7 +15,8 @@ const BottomSheet = ({ visible, onClose, children }) => {
     const { colorScheme } = useTheme();
 
     const translateY = useSharedValue(0);
-    const closeTranslateY = 600; // I didn't find a way to get the View height, fix this if possible !
+    const [contentHeight, setContentHeight] = useState(0);
+    const closeTranslateY = contentHeight || 900;
 
     const swipeStyle = useAnimatedStyle(() => {
         return {
@@ -51,13 +52,18 @@ const BottomSheet = ({ visible, onClose, children }) => {
                         <Pressable style={tw`absolute inset-0`} onPress={onClose} />
                         <View
                             style={[
-                                tw`bg-white dark:bg-gray-900 rounded-t-[20px] pt-3`,
+                                tw`bg-white dark:bg-gray-900 rounded-t-[20px] pt-3 max-h-[85%]`,
                                 { paddingBottom: insets.bottom },
-                            ]}>
+                            ]}
+                            // Detects View's height
+                            onLayout={(event) => {
+                                const { height } = event.nativeEvent.layout;
+                                setContentHeight(height);
+                            }}>
                             <View
                                 style={tw`w-10 h-1 bg-gray-300 dark:bg-gray-700 rounded-sm self-center mb-5`}
                             />
-                            {children}
+                                {children}
                         </View>
                     </Animated.View>
                 </GestureDetector>
